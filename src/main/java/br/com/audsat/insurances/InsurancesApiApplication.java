f8f84ffcac4e08d70cbe12e888c21924af7f3aa9
franciscgiongo <100457512+franciscgiongo@users.noreply.github.com>
2024-01-14 15:34:06 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/audsat/insurances/InsurancesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.audsat.insurances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/audsat/insurances/InsurancesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.audsat.insurances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
